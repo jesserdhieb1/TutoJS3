4bd65f6df123340028180c93628177c4d193c89f
jesser dhieb <dhieb.jesser@esprit.tn>
2022-07-20 00:58:46 +0200

    commit

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 
 const buttonEl = document.querySelector("#input-btn");
-let myLead=[];
+let myLead=["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl=document.querySelector("#input-el");
-
+const ulEl=document.querySelector("#ul-el");
 
 
 buttonEl.addEventListener("click",function (){
-    myLead.push("www.awesomelead.com");
+    myLead.push(inputEl.value);
+    inputEl.value="";
+    console.log(myLead);
 });
+
+for (let i = 0; i<myLead.length; i++){
+    ulEl.innerHTML+="<li>" + myLead[i] + "</li>";
+}
+
 
