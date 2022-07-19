0e72ed19343208be7beba20f54d63d026bc119ea
jesser dhieb <dhieb.jesser@esprit.tn>
2022-07-20 00:09:31 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
 
-let buttonEl = document.querySelector("#input-btn");
+const buttonEl = document.querySelector("#input-btn");
+let myLead=[];
+const inputEl=document.querySelector("#input-el");
+
+
+
 buttonEl.addEventListener("click",function (){
     console.log("Button clicked!");
 });
