63c2b0d94f11ea961db96a1e5736204251916b1b
jesser dhieb <dhieb.jesser@esprit.tn>
2022-07-20 04:49:25 +0200

    commit

--- index.js
+++ index.js
@@ -4,26 +4,42 @@ const buttonEl = document.querySelector("#input-btn");
 let myLead=[];
 const inputEl=document.querySelector("#input-el");
 const ulEl=document.querySelector("#ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLead"));
+const deleteBtn = document.querySelector("#delete-btn");
+if (leadsFromLocalStorage){
+    myLead=leadsFromLocalStorage;
+    render(myLead);
+}
 
-
-buttonEl.addEventListener("click",function (){
-    myLead.push(inputEl.value);
-    inputEl.value="";
-    renderLeads();
-});
-
-function renderLeads() {
+function render(Lead) {
     let listItems = "";
-    for (let i = 0; i < myLead.length; i++) {
+    for (let i = 0; i < Lead.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLead[i]}'>
-                    ${myLead[i]}
+                <a target='_blank' href='${Lead[i]}'>
+                    ${Lead[i]}
                 </a>
             </li>
         `
     }
     ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick",function (){
+    localStorage.clear();
+    ulEl.innerHTML ="";
+    myLead=[];
+})
+
+
+buttonEl.addEventListener("click",function (){
+    myLead.push(inputEl.value);
+    inputEl.value="";
+    localStorage.setItem("myLead",JSON.stringify(myLead));
+    render(myLead);
+    console.log(localStorage.getItem("myLead") );
+});
+
+
 
 
