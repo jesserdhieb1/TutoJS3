db766d60ce1e46d06f580d28ee83627e917f66d3
jesser dhieb <dhieb.jesser@esprit.tn>
2022-07-20 02:45:00 +0200

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 
 const buttonEl = document.querySelector("#input-btn");
-let myLead=["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLead=[];
 const inputEl=document.querySelector("#input-el");
 const ulEl=document.querySelector("#ul-el");
 
@@ -9,16 +9,21 @@ const ulEl=document.querySelector("#ul-el");
 buttonEl.addEventListener("click",function (){
     myLead.push(inputEl.value);
     inputEl.value="";
-    console.log(myLead);
+    renderLeads();
 });
 
-let listItems="";
-for (let i = 0; i<myLead.length; i++){
-    // let liEl = document.createElement("li");
-    // liEl.textContent=myLead[i];
-    // ulEl.append(liEl);
-    listItems+="<li>" + myLead[i] + "</li>";
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLead.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLead[i]}'>
+                    ${myLead[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems;
 }
-ulEl.innerHTML=listItems;
 
 
